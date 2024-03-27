a68538e76cbd73e5e3eefa46a1fe870233b73fe4
Konstantin Sandalov <k.sandalov@yadro.com>
2024-03-27 18:12:05 +0300

    UPD

--- LABS/sle_solve.cpp
+++ LABS/sle_solve.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <iostream>
-#include <omp.h>
 #include <random>
 #include <string>
 
@@ -58,18 +57,6 @@ public:
         return end - start;
     }
 
-    double solve_par()
-    {
-        double start{}, end{};
-        start = omp_get_wtime();
-        forward_par();
-        backward_par();
-        get_answer_par();
-        end = omp_get_wtime();
-        std::cout << "Elapsed parallel Gaussian time: " << end - start << " seconds\n";
-        return end - start;
-    }
-
     void forward()
     {
         for (int i = 0; i < m; ++i)
@@ -101,63 +88,6 @@ public:
             }
         }
     }
-    void forward_par()
-    {
-        for (int i = 0; i < m; ++i)
-        {
-#pragma omp parallel for
-            for (int j = 0; j < n; ++j)
-            {
-                coefs_tmp[i][j] = coefs_tmp[i][j] / coefs[i][i];
-            }
-
-            free_term_tmp[i] = free_term_tmp[i] / coefs[i][i];
-
-#pragma omp parallel for
-            for (int j = i + 1; j < n; ++j)
-            {
-                type coef = coefs_tmp[j][i] / coefs_tmp[i][i];
-                for (int k = 0; k < n; ++k)
-                {
-                    coefs_tmp[j][k] = coefs_tmp[j][k] - coefs_tmp[i][k] * coef;
-                }
-                free_term_tmp[j] = free_term_tmp[j] - free_term_tmp[i] * coef;
-            }
-
-#pragma omp parallel for
-            for (int k = 0; k < m; ++k)
-            {
-                for (int j = 0; j < n; ++j)
-                {
-                    coefs[k][j] = coefs_tmp[k][j];
-                }
-                free_term[k] = free_term_tmp[k];
-            }
-        }
-    }
-    void backward_par()
-    {
-        for (int i = m - 1; i >= 0; --i)
-        {
-#pragma omp parallel for
-            for (int j = n - 1; j >= 0; --j)
-            {
-                coefs_tmp[i][j] = coefs_tmp[i][j] / coefs[i][i];
-            }
-            free_term_tmp[i] = free_term_tmp[i] / coefs[i][i];
-
-#pragma omp parallel for
-            for (int j = i - 1; j >= 0; --j)
-            {
-                type coef = coefs_tmp[j][i] / coefs_tmp[i][i];
-                for (int k = n - 1; k >= 0; --k)
-                {
-                    coefs_tmp[j][k] = coefs_tmp[j][k] - coefs_tmp[i][k] * coef;
-                }
-                free_term_tmp[j] = free_term_tmp[j] - free_term_tmp[i] * coef;
-            }
-        }
-    }
     void backward()
     {
         for (int i = m - 1; i >= 0; --i)
@@ -182,15 +112,6 @@ public:
 
     void get_answer()
     {
-        for (int i = 0; i < n; ++i)
-        {
-            variables[i] = free_term_tmp[i];
-        }
-    }
-
-    void get_answer_par()
-    {
-#pragma omp parallel for
         for (int i = 0; i < n; ++i)
         {
             variables[i] = free_term_tmp[i];
@@ -254,36 +175,8 @@ bool performance_run(int task_size)
         free_term[i] = generator();
     }
 
-    omp_set_num_threads(4);
-
     Gauss method(task_size, task_size, coefs, free_term);
     double seq_time = method.solve();
-
-    Gauss method_par(task_size, task_size, coefs, free_term);
-    double par_time = method_par.solve_par();
-
-    for (int i = 0; i < task_size; ++i)
-    {
-        if (std::abs(method[i] - method_par[i]) > 1e-9)
-        {
-            std::cout << "Error while calculating\n";
-            delete[] (free_term);
-            for (int i = 0; i < task_size; ++i)
-            {
-                delete[] (coefs[i]);
-            }
-            delete[] (coefs);
-            return false;
-        }
-    }
-    std::cout << "All correct\n";
-
-    std::cout << "Speedup: " << seq_time / par_time << std::endl;
-#pragma omp parallel
-    {
-        if (omp_get_thread_num() == 0)
-            std::cout << "Num threads: " << omp_get_num_threads() << std::endl;
-    }
 
     delete[] (free_term);
     for (int i = 0; i < task_size; ++i)
