e2a557f1c0196a73cd12d00257e693a96a1ca3e0
SandalovKY <sandaloff.constantin@yandex.ru>
2025-03-17 02:42:49 +0300

    Update SR1

--- LABS/SR1/integral.cpp
+++ LABS/SR1/integral.cpp
@@ -1,72 +1,70 @@
 #include <iostream>
 #include <cmath>
-#include <ctime>
 #include <string>
 
 #include <chrono>
 
 class Solver {
 private:
-// double(*m_func)(double) ;
 
-double m_func(double x) {
-    double y;
-    y = 4. / sqrt(4. - pow((x), 2));
-    return y;
+double(*m_func)(float);
+
+void kahan_sum(double func_ret, double& sum, double& corr) {
+    const double y = func_ret - corr;
+    const double t = sum + y;
+    corr = (t - sum) - y;
+    sum = t;
 }
 
-double mid_rectangles(double a, double h, size_t q) {
+double mid_rectangles(float a, float h, size_t q) {
     double result{};
+    double c{};
 
     for(size_t i = 0; i < q; ++i) {
-        result += m_func(a + h * i + h / 2) * h;
+        kahan_sum(m_func(a + h * i + h / 2) * h, result, c);
     }
     return result;
 }
 
 public:
-Solver(double(*func)(double))
-// : m_func(func)
-{}
+Solver(double(*func)(float))
+: m_func(func) {}
 
-double integrate(double a, double b, size_t q) {
-    double h = (b - a) / q;
+float integrate(float a, float b, size_t q) {
+    const float h = (b - a) / q;
     return mid_rectangles(a, h, q);
 }
 };
 
-double func(double x) {
-    double y;
-    y = 4. / sqrt(4. - pow((x), 2));
-    return y;
+double func(float x) {
+    return 1. / sqrt(1. + pow(x, 7));
 }
 
-double refer = 2.094395;
+const double refer = 0.953824033354;
 
 int main(int argc, char** argv)
 {
+    std::cout << "Integral solver\n\n";
     size_t rects = 0;
     if (argc != 2) {
-        std::cerr << "Usage: integral.exe iterations [default is 1'000'000].\n";
-        rects = 1'000'000;
+        std::cerr << "Usage: integral.exe iterations [default is 1000000000].\n";
+        rects = 1000000000;
     } else {
         rects = std::stoll(argv[1]);
     }
 
-    std::cout << "Integral solver\n";
-    double a = 0;
-    double b = 1;
+    const float a = 0;
+    const float b = 1;
 
-    double  res;
     Solver  sol{func};
-    std::chrono::time_point t_start = std::chrono::high_resolution_clock::now();
+    
+    const std::chrono::time_point t_start = std::chrono::high_resolution_clock::now();
     double result = sol.integrate(a, b, rects);
-    std::chrono::time_point t_end = std::chrono::high_resolution_clock::now();
+    const std::chrono::time_point t_end = std::chrono::high_resolution_clock::now();
     
     const std::chrono::duration<double> elapsed_time = t_end - t_start;
 
     std::cout << "Elapsed time: " << elapsed_time.count() << std::endl;
-    
     std::cout << "Reference answer: " << refer << std::endl;
     std::cout << "Answer: " << result << std::endl;
     std::cout << "Delta: " << std::abs(refer - result) << std::endl;
