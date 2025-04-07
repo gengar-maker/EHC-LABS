1e3bfbc299cca395465845add5438ae7cff814b1
SandalovKY <sandaloff.constantin@yandex.ru>
2025-04-07 14:57:31 +0300

    Update win_run_script.bat

--- scripts/win_run_script.bat
+++ scripts/win_run_script.bat
@@ -4,5 +4,5 @@ set /a proc_aff=%2
 set prog=%~3
 set prog_arg=%4
 
-for /l %%i in (1 1 %iters%) do (START /wait /high /b /affinity %proc_aff% %prog% %prog_arg%) | findstr "time"
+for /l %%i in (1 1 %iters%) do (START /wait /high /b /affinity %proc_aff% %prog% %prog_arg%)
 pause
